916a38e32965f64815f36c3aed08d536f1e5fea4
wewe <wewe>
2022-10-19 16:32:13 +1200

    Initial commit

---
+++ src/wewe1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class wewe1 {
+    public static void main(String[] args) {
+        Scanner scam = new Scanner(System.in);
+        int a = scam.nextInt();
+        Scanner scan = new Scanner(System.in);
+        int b = scan.nextInt();
+        System.out.println("Hell0 U$Er*");
+        System.out.println("y0Ur a=" + a);
+        System.out.println("y0uR 8=" + b);
+        if (a > b && b > 10) {
+            System.out.println("0Ra9G3");
+        }
+        else if (b > a && a > 27) {
+            System.out.println("W@tt3rM3770N");
+
+
+        } else if (b < a || a < 27) {
+            System.out.println("3@K7le");
+        }
+    }
+}
